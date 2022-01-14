d31d4864784c6e8e9c138405006585efee0bd2c2
Adam Turowski <horosporo@gmail.com>
2022-01-14 19:34:13 +0100

    Add game and user models

---
+++ src/models/game.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const options = { timestapms: true };
+
+const GameSchema = new Schema({
+  title: String,
+  price: Number,
+  year: Number,
+  category: String,
+  tags: [String],
+  miniatureUrl: String,
+  description: String,
+}, options);
+
+const Game = mongoose.model("Game", GameSchema);
+
+module.exports = { Game, GameSchema };
---
+++ src/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { GameSchema } = require("./game");
+const { Schema } = mongoose;
+
+const options = { timestapms: true };
+
+const UserSchema = new Schema({
+  email: String,
+  password: String,
+  name: String,
+  surname: String,
+  admin: { type: Boolean, default: false },
+  cart: [GameSchema],
+}, options);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
